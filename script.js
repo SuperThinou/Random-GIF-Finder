a096b2caf46a75619150255cb210f2fe08b8be20
NathanBernard <nbx-off@outlook.fr>
2026-03-25 11:32:19 +0100

    style: added special btn style on GIF loading

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
 const img = document.querySelector("img");
 const randomGifBtn = document.getElementById("randomGifBtn");
 
-fetch(
-  "https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=vaporwave",
-)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (response) {
-    img.src = response.data.images.original.url;
-  });
+function loadGif() {
+  randomGifBtn.disabled = true;
+  randomGifBtn.classList.add("btn-disabled");
 
-randomGifBtn.addEventListener("click", () => {
   fetch(
     "https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=vaporwave",
   )
@@ -20,5 +13,16 @@ randomGifBtn.addEventListener("click", () => {
     })
     .then(function (response) {
       img.src = response.data.images.original.url;
+    })
+    .catch(function (error) {
+      console.error("Erreur :", error);
+    })
+    .finally(function () {
+      randomGifBtn.disabled = false;
+      randomGifBtn.classList.remove("btn-disabled");
     });
-});
+}
+
+loadGif();
+
+randomGifBtn.addEventListener("click", loadGif);
