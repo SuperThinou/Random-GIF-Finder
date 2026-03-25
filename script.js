25ba81a416770b0de14040f9bf4b802303959945
NathanBernard <nbx-off@outlook.fr>
2026-03-25 12:35:36 +0100

    feat: Made selector work

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
+const loader = document.querySelector(".loader");
 const img = document.querySelector("img");
 const randomGifBtn = document.getElementById("randomGifBtn");
-const loader = document.querySelector(".loader");
+const modeSelector = document.getElementById("mode");
 
 function loadGif() {
-  randomGifBtn.disabled = true;
-  randomGifBtn.classList.add("btn-disabled");
-  loader.classList.remove("hidden");
-  img.style.opacity = "0.3";
+  addLoadingStyle();
+  const url = getApiUrl();
+  console.log(url);
 
-  fetch(
-    "https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=vaporwave",
-  )
+  fetch(url)
     .then(function (response) {
       return response.json();
     })
@@ -21,13 +19,39 @@ function loadGif() {
       console.error("Erreur :", error);
     })
     .finally(function () {
-      randomGifBtn.disabled = false;
-      randomGifBtn.classList.remove("btn-disabled");
-      loader.classList.add("hidden");
-      img.style.opacity = "1";
+      removeLoadingStyle();
     });
+}
+
+function getApiUrl() {
+  const mode = modeSelector.value;
+
+  if (mode === "random") {
+    return `https://api.giphy.com/v1/gifs/random?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY`;
+  }
+
+  return `https://api.giphy.com/v1/gifs/translate?api_key=n5AUEykERIPKKOwuk3CDQUaRHE0vzvfY&s=${mode}`;
+}
+
+function addLoadingStyle() {
+  randomGifBtn.disabled = true;
+  randomGifBtn.classList.add("btn-disabled");
+  loader.classList.remove("hidden");
+  img.style.opacity = "0.3";
+}
+
+function removeLoadingStyle() {
+  randomGifBtn.disabled = false;
+  randomGifBtn.classList.remove("btn-disabled");
+  loader.classList.add("hidden");
+  img.style.opacity = "1";
 }
 
 loadGif();
 
 randomGifBtn.addEventListener("click", loadGif);
+
+modeSelector.addEventListener("change", () => {
+  img.src = "";
+  loadGif();
+});
